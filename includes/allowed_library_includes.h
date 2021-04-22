b15a50638c7dc926ff8858667aa70bc2cd5f995e
sfcdota <sfcdota@gmail.com>
2021-04-22 15:34:09 +0300

    first commit

---
+++ includes/allowed_library_includes.h
@@ -0,0 +1,25 @@
+#ifndef ALLOWED_LIBRARY_INCLUDES_H
+#define ALLOWED_LIBRARY_INCLUDES_H
+#include <cstdlib>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <dirent.h>
+#include <ctime>
+#include <cstring>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <exception>
+	#include <netdb.h> //????????
+#endif
