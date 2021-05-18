bfbaf397c5abc8890e7392dfe1580bdf98f8e9f1
vovvoy <ormonbaev.marsel@gmail.com>
2021-05-19 01:50:23 +0300

    half end parser

--- webserv/ServerConfig.hpp
+++ webserv/ServerConfig.hpp
@@ -1,9 +1,17 @@
-#ifndef WEBSERV_SERVERCONFIG_HPP_
-#define WEBSERV_SERVERCONFIG_HPP_
-#include "allowed_library_includes.hpp"
-#include <sys/errno.h>
+#ifndef SERVER_HPP_
+#define SERVER_HPP_
+#include <unistd.h>
+#include <cstring>
+#include <cstdlib>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
 struct error_page {
-  std::vector<int> error_codes;
+  int error_code;
   std::string error_path;
 };
 
@@ -17,14 +25,24 @@ struct location {
   std::string cgi_path;
 };
 
-struct ServerConfig {
-  int host;
+struct server {
+  std::string host;
   int port;
   std::vector<std::string> server_names;
   std::vector<error_page> error_pages;
-  size_t client_max_body_size;
+  int client_max_body_size;
   std::vector<location> locations;
 };
 
-
-#endif // WEBSERV_SERVERCONFIG_HPP_
+struct parser{
+	int							fd;
+	int							find;
+	std::string					str;
+    int							index;
+    bool						server_status;
+    bool						location_status;
+	std::vector<std::string>	string_arr;
+	int							res;
+	std::string					error_message;
+};
+#endif // SERVER_HPP_
