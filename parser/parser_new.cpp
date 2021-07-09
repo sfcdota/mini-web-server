37b7ab8276b96624e2e7585a250df3a3dad44a5a
sfcdota <sfcdota@gmail.com>
2021-07-09 21:21:16 +0300

    Revert "a litle bit of new parser"
    This reverts commit 7a322b04

--- parser/parser_new.cpp
+++ parser/parser_new.cpp
@@ -10,13 +10,12 @@ int main() {
   server serv;
   location loc;
   char *line;
-  pars.fd = open("../server.conf", O_RDONLY);
+
+//  init_parser(pars);
+//	init_serv(serv);
+  pars.fd = open("./server.conf", O_RDONLY);
   while ((pars.res = get_next_line(pars.fd, &line)) > -1) {
 	line = whitespace(line);
-	if (*line == '#')
-		continue;
-	if (strncmp(line, "server", 6) == 0)
-		line = line + 6;
 	std::cout << line << std::endl;
 	if (!pars.res)
 	  break;
