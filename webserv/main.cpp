27661aba21c707286d5d8307d5f169528055ac2f
sfcdota <sfcdota@gmail.com>
2021-07-17 16:48:33 +0300

    tmp working cgi pipe

--- webserv/main.cpp
+++ webserv/main.cpp
@@ -21,34 +21,22 @@ SERVER_SOFTWARE=kekers228/v4.20
  */
 
 
-char** mapToCString(std::map<std::string, std::string> &tmpEnv) {
-  char **_env;
-  _env = reinterpret_cast<char **>(calloc(tmpEnv.size() + 1, sizeof(char *)));
-  std::map<std::string, std::string>::iterator it = tmpEnv.begin();
-  for (int index = 0; it != tmpEnv.end(); ++it, ++index) {
-    _env[index] = strdup((it->first + it->second).c_str());
-    std::cout << _env[index] << std::endl;
-  }
-  return _env;
-}
-
-
-
-std::string testCGI() {
+char** mapToCString() {
   chdir("/Users/cbach/CLionProjects/webserv/webserv/site/");
   std::map<std::string, std::string> tmpEnv;
   tmpEnv["CONTENT_LENGTH="] = "5";
   tmpEnv["CONTENT_TYPE="] = "test/file";
   tmpEnv["GATEWAY_INTERFACE="] = "CGI/1.1";
-  tmpEnv["PATH_INFO="] = "/directory/youpi.bla";
-  tmpEnv["PATH_TRANSLATED="] = "/Users/cbach/CLionProjects/webserv/webserv/site/directory/youpi.bla";
+//  tmpEnv["PATH_INFO="] = "directory/youpi.bla";
+  tmpEnv["PATH_INFO="] = "YoupiBanane/directory/youpi.bla";
+  tmpEnv["PATH_TRANSLATED="] = "/Users/cbach/CLionProjects/webserv/webserv/site/YoupiBanane/directory/youpi.bla";
   tmpEnv["QUERY_STRING="] = "";
 //  tmpEnv["REDIRECT_STATUS"] = "200";
   tmpEnv["REMOTE_ADDR="] = "174.127.0.0" ;
 //  tmpEnv["REMOTE_IDENT="] = "basic";
 //  tmpEnv["REMOTE_USER="] = "?????";
   tmpEnv["REQUEST_METHOD="] = "POST";
-  tmpEnv["REQUEST_URI="] = "/YoupiBanane/directory/youpi.bla";
+  tmpEnv["REQUEST_URI="] = "YoupiBanane/directory/youpi.bla";
   tmpEnv["SCRIPT_NAME="] = "cgi/cgi_tester";
   tmpEnv["SCRIPT_FILENAME="] = "/Users/cbach/CLionProjects/webserv/webserv/site/cgi/cgi_tester";
 
@@ -59,45 +47,66 @@ std::string testCGI() {
   tmpEnv["SERVER_PROTOCOL="] = "HTTP/1.1";
   tmpEnv["SERVER_SOFTWARE="] = "kekers228/v4.20";
 //	tmpEnv["HTTP_X_SECRET_HEADER_FOR_TEST="] = "1";
-  char **envp = mapToCString(tmpEnv);
+  tmpEnv["HTTP_X_SECRET_HEADER_FOR_TEST="] = "1";
+  char **_env;
+  _env = reinterpret_cast<char **>(calloc(tmpEnv.size() + 1, sizeof(char *)));
+  std::map<std::string, std::string>::iterator it = tmpEnv.begin();
+  for (int index = 0; it != tmpEnv.end(); ++it, ++index) {
+    _env[index] = strdup((it->first + it->second).c_str());
+    std::cout << _env[index] << std::endl;
+  }
+  return _env;
+
+}
+
+
+
+std::string testCGI() {
+  char **envp = mapToCString();
   pid_t pid;
   int status;
   int fd[2];
+  int stdoutt = dup(STDOUT_FILENO);
+  int stdinn = dup(STDIN_FILENO);
   pipe(fd);
   pid = fork();
-//  dup2(fd[1], STDOUT_FILENO);
 
-dup2(STDIN_FILENO, fd[0]);
   if (pid == -1) {
     throw std::runtime_error("error");
   } else if (pid == 0) {
+//    dup2(fd[1], STDOUT_FILENO);
     dup2(fd[0], STDIN_FILENO);
-    dup2(fd[1], STDOUT_FILENO);
 //    close(fd[1]);
+    dup2(fd[1], STDOUT_FILENO);
     std::string hui = "/Users/cbach/CLionProjects/webserv/webserv/site/cgi/cgi_tester";
-    char **kek = new char*[1];
-    kek[0] = "/Users/cbach/CLionProjects/webserv/webserv/site/cgi/cgi_tester";
-//    char* kek = const_cast<char *>(hui.c_str());
-//    dup2(STDIN_FILENO, STDOUT_FILENO);
+    char **kek = new char*[2];
+    kek[0] = strdup("/Users/cbach/CLionProjects/webserv/webserv/site/cgi/cgi_tester");
+    kek[1] = NULL;
     if (execve(kek[0], kek, envp) == -1) {
-      std::cout << errno << " " << strerror(errno) << std::endl;
-      throw std::runtime_error("Error: execve");
+      write(2, strerror(errno), strlen(strerror(errno)));
+      write(2, "hui\n", 4);
+
     }
-    std::cout << "PRIVETIKI" << std::endl;
+
     exit(0);
   }
-//  close(fd[0]);
-//  dup2(fd[1], STDOUT_FILENO);
-//  std::cout << "POST /directory/youpi.bla HTTP/1.1\r\nContent-Length: 5\r\nContent-Type: test/file\r\n\r\neeeee\r\n\r\n";
-  write(fd[0], "eeeee", 5);
-  waitpid(pid, &status, 0);
-//  dup2(fd[1], STDIN_FILENO);
-  std::string result_dst;
-  char *hui = (char*)malloc(10000);
-  size_t statuss = read(fd[1], hui, 100);
-  std::cout << hui;
-  close(fd[1]);
-  return "ha";
+  else {
+    std::string pizda;
+    dup2(fd[1], STDOUT_FILENO);
+    close(fd[1]);
+    dup2(fd[0], STDIN_FILENO);
+    close(fd[0]);
+    std::cout << "12123121231" << std::endl;
+    dup2(stdoutt, STDOUT_FILENO);
+    std::cout << "end" << std::endl;
+    wait(NULL);
+//    std::string tmp;
+//    while(std::cin >> tmp)
+//      pizda += tmp;
+//    dup2(stdinn, STDIN_FILENO);
+std::getline(std::cin, pizda, '\0');
+    return pizda;
+  }
 }
 
 
@@ -105,9 +114,10 @@ dup2(STDIN_FILENO, fd[0]);
 int main(int argc, char **argv) {
   if (argc > 1) // for tests
     exit(0);
-
-  std::string output = testCGI();
-  std::cout << output << std::endl;
+  std::string res = testCGI();
+  std::cout << res;
+//  std::string output = testCGI();
+//  std::cout << output << std::endl;
 //  ServerConfig config = parsConf();
 //  const ServerConfig DEFAULT_CONFIG =
 //      {0, 8080, std::string(),
